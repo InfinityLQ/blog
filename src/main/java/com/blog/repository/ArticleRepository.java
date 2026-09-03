06b6dd85db96b9cec3a15664ecdd3ff1f209b211
InfinityLQ <3035763773@qq.com>
2026-09-03 23:54:28 +0300

    add ArticleRepository for database operations

---
+++ src/main/java/com/blog/repository/ArticleRepository.java
@@ -0,0 +1,15 @@
+package com.blog.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.blog.entity.Article;
+
+import java.util.List;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+    List<Article> findByCategory(String category);
+    List<Article> findByOrderByCreateTimeDesc();
+}
